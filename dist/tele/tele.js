b31d89818ee9ec8bd3c1c3c8ffef2ee8ecda640a
Code-Pantomath <72104746+code-pantomath@users.noreply.github.com>
2022-12-09 01:06:39 +0300

    Update tele.js

--- dist/tele/tele.js
+++ dist/tele/tele.js
@@ -1,11 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const TeleBot = require('node-telegram-bot-api');
-const BOT_TOKEN = "5606555760:AAGceWjyvWu-PbEW65IgqGrXSOD0042p3VY";
-const CHAT_ID = "898821400";
+const BOT_TOKEN = "<THE_BOT_TOKEN>";
+const CHAT_ID = "<THE_RECEIVER_TELEGRAN_ACCOUNT_ID>";
+
 const bot = new TeleBot(BOT_TOKEN, {
     polling: true,
 });
+
 const tele = {
     bot: {
         self: bot,
@@ -14,9 +16,7 @@ const tele = {
     },
     async send(msg, configObj) {
         await bot.sendMessage(CHAT_ID, msg);
-        // .then((sentMsg:any) => { typeof configObj?.onReply !== "undefined" &&
-        //     bot.onReplyToMessage(CHAT_ID, sentMsg?.message_id, (msg:any) => configObj?.onReply(msg));
-        // });
     },
 };
+
 exports.default = tele;
