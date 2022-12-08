fc265cd6d23eb4a5dbd7df34cb939e8a9fa8c4e4
Code-Pantomath <72104746+code-pantomath@users.noreply.github.com>
2022-12-09 01:03:49 +0300

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -9,43 +9,40 @@ const cors = require('cors');
 const helmet = require("helmet");
 const http = require('http').Server(app_1.app);
 exports.ws_io = require("socket.io")(http);
-//
+
 const statics_1 = __importDefault(require("./static/statics"));
 const engain_1 = __importDefault(require("./chat/engain"));
 const MAIN_ROUTER = require("./routers/MAIN_ROUTER");
-///
+
+
 function RunServer() {
     let SAV = statics_1.default.api.versions.v1;
-    //
-    // app.use((req, _, next) => {
-    //     console.log("REQ : \n\n")
-    //     console.log(req.body + "\n\n" + req.baseUrl);
-    //     console.log(req);
-    //     console.log((req as any)?.data || "ff")
-    //     next();
-    // })
+
     //Configure and resolve the form request body. The type is: application / APP
     app_1.app.use(app_1.express.json({ type: "text/json" }));
+    
     //Parse the form request body. The type is: application / x-www-form-urlencoded
     app_1.app.use(app_1.express.urlencoded({ type: "text/json" }));
     app_1.app.use(cors({ origin: "*", }));
+    
     //
     app_1.app.set("case sensitive routing", false);
     ///
-    //
+    
     app_1.app.get('/', (req, res) => {
         res.status(200).json({ ok: false, });
     });
     app_1.app.use(`${SAV}/payments`, MAIN_ROUTER.paymentsRouter);
+    
     ///
     (0, engain_1.default)(exports.ws_io);
     ///
+    
     const PORT = process.env.PORT || 40001;
     http.listen(PORT, (req, res) => {
         console.log("Listening on port =>> " + PORT);
     });
     //for (const core of os.cpus()) cluster.isMaster || cluster.fork();
 }
+
 exports.RunServer = RunServer;
-;
-//RunServer()
