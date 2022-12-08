737dcd9bcfeac9cf7419155b79e3e3b749354303
Code-Pantomath <72104746+code-pantomath@users.noreply.github.com>
2022-12-09 01:23:52 +0300

    Update engain.js

--- dist/chat/engain.js
+++ dist/chat/engain.js
@@ -9,60 +9,42 @@ let isRunChatTeleBotListenerAllowed = false;
 function RunChatTeleBotListener(bot, io_ws, ws_io) {
     isRunChatTeleBotListenerAllowed &&
         bot.self.on('message', (msg) => {
-            // const chatId = msg.chat.id;
             const trimmedMsg = (msg?.text + "").toLowerCase().trim().replace(/ /g, '');
-            // send a message to the chat acknowledging receipt of their message
             msg?.text === "check" && bot.self.sendMessage(bot.admin_chat_id, 'Received your message');
             const receivedSocketIdFromSupportMsg = msg?.text?.split("__")?.at(1);
-            //// console.log("Socket ID ", receivedSocketIdFromSupportMsg);
-            //// console.log("new msg from admin : ", msg);
             const finalResponceMsg = (msg?.text + "").replace(`${receivedSocketIdFromSupportMsg}`, "").replace("__", '').replace("aofq", "").trim();
-            if (trimmedMsg.includes("aofq") && trimmedMsg.includes("__")) {
-                //// console.log(trimmedMsg, "Trimmed msg");
-                // io_ws?.emit("Support_Msg", {msg: finalResponceMsg});
+            
+        if (trimmedMsg.includes("aofq") && trimmedMsg.includes("__")) {
                 io_ws?.emit("Support_Msg", { msg: finalResponceMsg });
-                // io_ws.removeAllListeners();
-            }
-            ;
+            };
         });
 }
+
 function RunChatWSEngain(io) {
     io.on("connection", (socket) => {
         // socket.removeAllListeners();
-        //// console.log(socket.id);
         isRunChatTeleBotListenerAllowed = true;
         RunChatTeleBotListener(tele_1.default?.bot, socket, io);
         ///
+        
         socket.on("Get_AllMsgs", (({ userId }) => {
-            //// console.log("Get_AllMsgs ", userId);
             if (!userId)
                 return;
             db_1.default.connect().then(() => {
                 db_1.default.docs.chats.getAllMsgs(userId).then(msgs => {
-                    //// console.log(msgs);
                     socket.emit("Send_AllMsgs", { msgs });
                 });
             });
             // await db.disconnect();
         }));
+        
         socket.on("User_Msg", (msgDataPyload) => {
-            // //// console.log(msg);
-            const { msg, userIp, userId, userEmail } = msgDataPyload;
-            //// console.log("UID : ", userId)
-            tele_1.default.send((` 
-                QUESTION :
-                
-                |WSID|${socket.id}|WSID|
-                |IP|${userIp}|IP|
-                |USERID|${userId}|USERID|
-                |EMAIL|${userEmail}|EMAIL|
-                
-                
-                ${msg}
-                `)
+//             const { msg, userIp, userId, userEmail } = msgDataPyload;
+            
+            tele_1.default.send(JSON.stringify(msgDataPyload))
             // ,{
-            //     onReply: (message:any) => {
-            //         console.log('OK. I\'ll search for %s', message?.text);
+            //     onReply: (msg) => {
+            //         console.log(msg);
             //     },
             // }
             ).then(() => tele_1.default.send(socket.id)).catch(err => console.error(err));
@@ -78,16 +60,14 @@ function RunChatWSEngain(io) {
                                     date: new Date().getDate(),
                                 },
                             });
-                            // .finally(() => db.disconnect()); // NOT GOOD PRACTICE!!
+                            // .finally(() => db.disconnect()); // NOT A GOOD PRACTICE!!
                             // .finally(() => 0);
                         });
                 });
             }
         });
         socket.on("Support_Msg__Received", (dataObj) => {
-            // if (!userId) return;
             const { msg, userId } = dataObj;
-            //// console.log("Support_Msg__Received ", userId);
             if (userId) {
                 db_1.default.docs.chats.addMsg({
                     userId,
@@ -100,11 +80,6 @@ function RunChatWSEngain(io) {
             }
         });
         socket.on("disconnect", () => {
-            // socket.removeAllListeners('User_Msg');
-            // socket.removeAllListeners("Support_Msg__Received");
-            // socket.removeAllListeners('disconnect');
-            // io.removeAllListeners('connection');
-            // socket.removeAllListeners();
             isRunChatTeleBotListenerAllowed = false;
             db_1.default.disconnect();
             tele_1.default.bot.self?.removeListener("message");
