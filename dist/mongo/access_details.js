e04074c1ee01dbe40b3ae281b8b19865f1a09d65
Code-Pantomath <72104746+code-pantomath@users.noreply.github.com>
2022-12-09 01:09:18 +0300

    Update access_details.js

--- dist/mongo/access_details.js
+++ dist/mongo/access_details.js
@@ -1,4 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_CONNECTION_STRING = void 0;
-exports.DB_CONNECTION_STRING = "mongodb+srv://admin:Rashad712468899_@cuc-cluster-01.da1odaw.mongodb.net/?retryWrites=true&w=majority";
+exports.DB_CONNECTION_STRING = "<MONGODB_SECRET_CONNECTION_STRING>";
